ae00373251de6d9256d17aae747c15ba7cea14b0
Juan Sebastián Franco <regsebas@gmail.com>
2026-03-29 01:28:44 -0500

    feature: billing

--- LAGS_jam/Assets/Code/Scripts/Features/FinalDayHandler.cs
+++ LAGS_jam/Assets/Code/Scripts/Features/FinalDayHandler.cs
@@ -11,6 +11,13 @@ public class FinalDayHandler : MonoBehaviour
     [SerializeField] private TMP_Text prototypeTextDay;
     [SerializeField] private AnimationUIController animationUIController;
     [SerializeField] private CallerSceneLoader callerSceneLoader;
+    [Header("-Dialogs-")]
+    [SerializeField] private DialogSheet dialogRenta;
+    [SerializeField] private DialogSheet dialogEscuela;
+    [SerializeField] private DialogSheet dialogTransporte;
+    [SerializeField] private DialogSheet dialogMercado;
+    [SerializeField] private DialogSheet dialogDeudatotal;
+    [SerializeField] private DialogSheet dialogMiDinero;
 
     private void OnEnable() => GameStateContext.GameStateMediator.Subscribe(TypeGameState.EndDay, OpenEndDay);
 
@@ -25,27 +32,27 @@ public class FinalDayHandler : MonoBehaviour
         ClearTexts();
         yield return new WaitForSeconds(1.2f);
         var clone = Instantiate(prototypeTextDay, content);
-        clone.text = $"Renta:{-10}$";
+        clone.text = dialogRenta.Model.GetDialog().Replace("#", "-10");//$"Renta:{-10}$";
         clone.transform.SetAsLastSibling();
         yield return new WaitForSeconds(1.2f);
         var clone2 = Instantiate(prototypeTextDay, content);
-        clone2.text = $"Escula:{-10}$";
+        clone2.text = dialogEscuela.Model.GetDialog().Replace("#", "-10");
         clone2.transform.SetAsLastSibling();
         yield return new WaitForSeconds(1.2f);
         var clone3 = Instantiate(prototypeTextDay, content);
-        clone3.text = $"Transporte:{-10}$";
+        clone3.text = dialogTransporte.Model.GetDialog().Replace("#", "-10");
         clone3.transform.SetAsLastSibling();
         yield return new WaitForSeconds(1.2f);
         var clone4 = Instantiate(prototypeTextDay, content);
-        clone4.text = $"Mercado:{-10}$";
+        clone4.text = dialogMercado.Model.GetDialog().Replace("#", "-10");
         clone4.transform.SetAsLastSibling();
         yield return new WaitForSeconds(1.2f);
         var clone5 = Instantiate(prototypeTextDay, content);
-        clone5.text = $"Deuda total:{-40}$";
+        clone5.text = dialogDeudatotal.Model.GetDialog().Replace("#","-40");
         clone5.transform.SetAsLastSibling();
         yield return new WaitForSeconds(1.2f);
         var clone6 = Instantiate(prototypeTextDay, content);
-        clone6.text = $"Mi dinero :{MoneyDataService.GetMoney()} {-40} = {MoneyDataService.GetMoney()-40}$";
+        clone6.text = dialogMiDinero.Model.GetDialog().Replace("#",$"{MoneyDataService.GetMoney()} {-40} = {MoneyDataService.GetMoney() - 40}");
         clone6.transform.SetAsLastSibling();
         MoneyDataService.RemoveMoney(40);
         yield return new WaitForSeconds(1.2f);
